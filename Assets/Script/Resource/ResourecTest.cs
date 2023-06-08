615f66d75e23bc3fea8de60d840a349f061a7241
我的代码没有bug <1187390456@qq.com>
2023-06-08 16:48:03 +0800

    Build AssetBundleConfig data in bundle

---
+++ Assets/Script/Resource/ResourecTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor;
+using UnityEngine;
+
+public class ResourecTest : MonoBehaviour
+{
+
+    void Start()
+    {
+        AssetBundle abBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Data");
+        TextAsset textAsset = abBundle.LoadAsset<TextAsset>("AssetBundleConfig.bytes");
+        using MemoryStream ms = new MemoryStream(textAsset.bytes);
+        BinaryFormatter bf = new BinaryFormatter();
+        AssetBundleConfig cf = bf.Deserialize(ms) as AssetBundleConfig;
+        string targetPath = "Assets/GameData/Prefabs/Attack.prefab";
+        uint crc = Crc32.GetCrc32(targetPath);
+        ABBase abBase = null;
+        foreach (ABBase item in cf.ABList)
+        {
+            if (item.Crc == crc) abBase = item;
+        }
+        // 加载依赖项
+        foreach (var dp in abBase.ABDependence)
+        {
+            AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + dp);
+        }
+
+        // 加载自身
+
+        AssetBundle bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + abBase.ABName);
+        GameObject go = bundle.LoadAsset<GameObject>(abBase.AssetName);
+        Instantiate(go);
+    }
+
+
+}
